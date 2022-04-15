00dddc76591fc769a9f97fa5504ab6a881d3af14
stargazing4122 <jadenyeiden@gmail.com>
2022-04-15 15:33:46 -0500

    fetch with token and without token added: utils

---
+++ src/utils/fetch.ts
@@ -0,0 +1,50 @@
+type ReqMethod =
+  | 'GET'
+  | 'POST'
+  | 'PUT'
+  | 'DELETE'
+
+
+export const fetchWithoutToken = (endPoint: string, data: any, method: ReqMethod = 'GET') => {
+
+  const url: string = `${ process.env.REACT_APP_API}${endPoint}`;
+
+  if( method === 'GET' ) {
+    return fetch( url );
+  } else {
+    return fetch( url, {
+      method,
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify( data ),
+    });
+  }
+
+}
+
+
+export const fetchWithToken = ( endPoint: string, data: any, method: ReqMethod = 'GET') => {
+
+  const url: string = `${ process.env.REACT_APP_API}${endPoint}`;
+  const token: string = localStorage.getItem('token-enrollment-mern') || '';
+
+  if( method === 'GET' ) {
+    return fetch( url, {
+      method,
+      headers: {
+        'x-token': token,
+      },
+    })
+  } else {
+    return fetch( url, {
+      method,
+      headers: {
+        'Content-type': 'application/json',
+        'x-token': token,
+      },
+      body: JSON.stringify( data ),
+    })
+  }
+
+}
